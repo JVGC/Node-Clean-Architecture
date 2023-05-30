659186e58ffa6520cb2305aad4460e1560f1946c
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-05-30 11:09:34 -0300

    UserRoles interface

--- src/domain/models/user.ts
+++ src/domain/models/user.ts
@@ -6,4 +6,12 @@ export interface User{
     email: string
     password: string
     company: Company
+    role: UserRoles
+}
+
+// DECISION: All users can CRUD Assets and Units. The different is that superAdmin can do that for different companies
+export enum UserRoles{
+    superAdmin = 'SuperAdmin', // DECISION: Can Create companies, and other user for different companies
+    admin = 'Admin', // DECISION: Cannot create companies, and only can create users for its own company.
+    user = 'User' // DECISION: Cannot get any information about other users, and other companies.
 }
