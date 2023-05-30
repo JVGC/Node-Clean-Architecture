261b75c9e905d933a127135a2dc4c573f50a1c5f
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-05-30 11:15:18 -0300

    Status and HealthLevel default decisions

--- src/domain/models/asset.ts
+++ src/domain/models/asset.ts
@@ -8,8 +8,8 @@ export interface Asset{
     description: string;
     model: string; // DECISION: Não criar uma interface para o Model pois não há muitas informações deste nesse momento. Mas caso seja necessário seria bom criar e fazer um relation aqui
     owner: User; // DUVIDA: Esse Owner é um usuário do sistema?
-    status: AssetsStatus;
-    healthLevel: number
+    status: AssetsStatus; // DECISION: Default is RUNNING
+    healthLevel: number // DECISION: Default is 100%
     unit: Unit
 }
 
