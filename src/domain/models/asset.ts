8f63c82c5a35a58af2ce5acbba76f12d4a380746
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-05-31 19:13:14 -0300

    Setting owner as a simple string

--- src/domain/models/asset.ts
+++ src/domain/models/asset.ts
@@ -1,5 +1,4 @@
 import { Unit } from './unit';
-import { UserModel } from './user';
 
 
 // TODO: Adicionar image
@@ -8,7 +7,7 @@ export interface Asset{
     name: string;
     description: string;
     model: string; // DECISION: Não criar uma interface para o Model pois não há muitas informações deste nesse momento. Mas caso seja necessário seria bom criar e fazer um relation aqui
-    owner: UserModel; // DUVIDA: Esse Owner é um usuário do sistema?
+    owner: string; // DUVIDA: Esse Owner é um usuário do sistema?
     status: AssetsStatus; // DECISION: Default is RUNNING
     healthLevel: number // DECISION: Default is 100%
     unit: Unit
