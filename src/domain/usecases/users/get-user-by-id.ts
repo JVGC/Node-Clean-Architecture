a246cafa1fba1d2d0251d38412bd5a82c4fb38fe
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-05-31 11:51:21 -0300

    Implementing GetUserById

--- src/domain/usecases/users/get-user-by-id.ts
+++ src/domain/usecases/users/get-user-by-id.ts
@@ -1,11 +1,14 @@
-import { UserModel } from "../../models/user";
+import { UserNotFoundError } from "../../errors";
+import { UserModelResponse } from "../../models/user";
 import { UserRepository } from "../../protocols/repositories/user-repository";
 
 export class GetUserByIdUseCase {
     constructor(
         private readonly userRepository: UserRepository
     ){}
-    async get(user_id: string): Promise<UserModel>{
-      
+    async get(user_id: string): Promise<UserModelResponse>{
+        const user = await this.userRepository.getById(user_id)
+        if(!user) throw new UserNotFoundError()
+        return user
     }
 }
