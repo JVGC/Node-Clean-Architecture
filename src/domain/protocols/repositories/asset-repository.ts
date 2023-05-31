de539cd8f45907200f274b1edcf8e7331c469667
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-05-31 19:19:18 -0300

    Asset repository interface

--- src/domain/protocols/repositories/asset-repository.ts
+++ src/domain/protocols/repositories/asset-repository.ts
@@ -1,2 +1,11 @@
+import { AssetModelResponse } from "../../models/asset"
+import { CreateAssetParams } from "../../usecases/asset/create-asset"
+import { UpdateAssetParams } from "../../usecases/asset/update-asset"
+
 export interface AssetRepository{
+    create: (data: CreateAssetParams) => Promise<AssetModelResponse>
+    getById: (id: string) => Promise<AssetModelResponse | null>
+    getMany: () => Promise<AssetModelResponse[]>
+    deleteById: (id: string) => Promise<boolean>
+    update: (id: string, updateData: UpdateAssetParams) => Promise<AssetModelResponse | null>
 }
