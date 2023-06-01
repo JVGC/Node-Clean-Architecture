3a0bf73c2c3beb5d4e5727650d30cb3a7cf289d1
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-06-01 08:54:08 -0300

    Setting middleware for all the routes

---
+++ src/main/express/middlewares/auth-middlewares.ts
@@ -0,0 +1,4 @@
+import { adaptAuthMiddleware } from "../../adapters/express-adapter";
+import { makeAuthMiddleware } from "../../factories/middlewares-factory";
+
+export const authMiddleware = adaptAuthMiddleware(makeAuthMiddleware())
--- src/main/express/routes.ts
+++ src/main/express/routes.ts
@@ -1,11 +1,12 @@
 import { Express, Router } from 'express'
-import { adaptAuthMiddleware, adaptRoute } from '../adapters/express-adapter'
+import { adaptRoute } from '../adapters/express-adapter'
 import { makeCreateAsset, makeDeleteAssetById, makeGetAssetById, makeListAssets, makeUpdateAsset } from '../factories/asset-controllers-factory'
 import { makeLogin } from '../factories/auth-controllers-factory'
 import { makeCreateCompany, makeDeleteCompanyById, makeGetCompanyById, makeListCompanies, makeUpdateCompany } from '../factories/company-controllers-factory'
-import { makeAuthMiddleware } from '../factories/middlewares-factory'
 import { makeCreateUnit, makeDeleteUnitById, makeGetUnitById, makeListUnits, makeUpdateUnit } from '../factories/unit-controllers-factory'
 import { makeCreateUser, makeDeleteUserById, makeGetUserById, makeListUsers, makeUpdateUser } from '../factories/user-controllers-factory'
+import { authMiddleware } from './middlewares/auth-middlewares'
+import { adminMiddleware, superAdminMiddleware } from './middlewares/permissions-middleware'
 
 export default (app: Express): void => {
 
@@ -14,27 +15,27 @@ export default (app: Express): void => {
 
     router.post('/login', adaptRoute(makeLogin()))
 
-    router.post('/company', adaptRoute(makeCreateCompany()))
-    router.get('/company/:id', adaptRoute(makeGetCompanyById()))
-    router.delete('/company/:id', adaptRoute(makeDeleteCompanyById()))
-    router.get('/company', adaptAuthMiddleware(makeAuthMiddleware()), adaptRoute(makeListCompanies()))
-    router.patch('/company/:id', adaptRoute(makeUpdateCompany()))
+    router.post('/company', authMiddleware, superAdminMiddleware, adaptRoute(makeCreateCompany()))
+    router.delete('/company/:id', authMiddleware, superAdminMiddleware, adaptRoute(makeDeleteCompanyById()))
+    router.get('/company', authMiddleware, superAdminMiddleware, adaptRoute(makeListCompanies()))
+    router.patch('/company/:id', authMiddleware, superAdminMiddleware, adaptRoute(makeUpdateCompany()))
 
-    router.post('/user', adaptRoute(makeCreateUser()))
-    router.get('/user/:id', adaptRoute(makeGetUserById()))
-    router.delete('/user/:id', adaptRoute(makeDeleteUserById()))
-    router.get('/user', adaptRoute(makeListUsers()))
-    router.patch('/user/:id', adaptRoute(makeUpdateUser()))
+    router.get('/company/:id', authMiddleware, adminMiddleware, adaptRoute(makeGetCompanyById()))
+    router.post('/user', authMiddleware, adminMiddleware, adaptRoute(makeCreateUser()))
+    router.get('/user/:id', authMiddleware, adminMiddleware, adaptRoute(makeGetUserById()))
+    router.delete('/user/:id', authMiddleware, adminMiddleware, adaptRoute(makeDeleteUserById()))
+    router.get('/user', authMiddleware, adminMiddleware, adaptRoute(makeListUsers()))
+    router.patch('/user/:id', authMiddleware, adminMiddleware, adaptRoute(makeUpdateUser()))
 
-    router.post('/unit', adaptRoute(makeCreateUnit()))
-    router.get('/unit/:id', adaptRoute(makeGetUnitById()))
-    router.delete('/unit/:id', adaptRoute(makeDeleteUnitById()))
-    router.get('/unit', adaptRoute(makeListUnits()))
-    router.patch('/unit/:id', adaptRoute(makeUpdateUnit()))
+    router.post('/unit', authMiddleware, adaptRoute(makeCreateUnit()))
+    router.get('/unit/:id', authMiddleware, adaptRoute(makeGetUnitById()))
+    router.delete('/unit/:id', authMiddleware, adaptRoute(makeDeleteUnitById()))
+    router.get('/unit', authMiddleware, adaptRoute(makeListUnits()))
+    router.patch('/unit/:id', authMiddleware, adaptRoute(makeUpdateUnit()))
 
-    router.post('/asset', adaptRoute(makeCreateAsset()))
-    router.get('/asset/:id', adaptRoute(makeGetAssetById()))
-    router.delete('/asset/:id', adaptRoute(makeDeleteAssetById()))
-    router.get('/asset', adaptRoute(makeListAssets()))
-    router.patch('/asset/:id', adaptRoute(makeUpdateAsset()))
+    router.post('/asset', authMiddleware, adaptRoute(makeCreateAsset()))
+    router.get('/asset/:id', authMiddleware, adaptRoute(makeGetAssetById()))
+    router.delete('/asset/:id', authMiddleware, adaptRoute(makeDeleteAssetById()))
+    router.get('/asset', authMiddleware, adaptRoute(makeListAssets()))
+    router.patch('/asset/:id', authMiddleware, adaptRoute(makeUpdateAsset()))
   }
