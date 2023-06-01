730af98678ac6a4265d1d8b10eeeecba9beaeae3
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-06-01 15:44:56 -0300

    Swagger express ui + company routes docs

--- src/main/express/setup-express.ts
+++ src/main/express/setup-express.ts
@@ -1,11 +1,17 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
 import { bodyParser, contentType, cors } from "./middlewares/basic-middlewares"
 import setupRoutes from './routes'
+import swaggerFile from './swagger.json'
 
+console.log(swaggerFile)
 const expressApp = express()
+
+expressApp.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 expressApp.use(bodyParser)
 expressApp.use(cors)
 expressApp.use(contentType)
 setupRoutes(expressApp)
+
 
 export default expressApp
