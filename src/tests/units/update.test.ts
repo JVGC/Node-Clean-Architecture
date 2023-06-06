96127f606d69c16071f91d0b891afe702f17c6d1
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-06-06 16:04:08 -0300

    Update Unit Tests

---
+++ src/tests/units/update.test.ts
@@ -0,0 +1,122 @@
+
+import expressApp from '../../main/express/setup-express'
+
+import { faker } from '@faker-js/faker'
+import { UserRoles } from '@prisma/client'
+import request from 'supertest'
+import { UnitNotFoundError } from '../../domain/errors'
+import { FactoryCompany } from '../factories/company'
+import { FactoryUnit } from '../factories/unit'
+import { FactoryUser } from '../factories/user'
+
+describe('Update Unit Tests', () => {
+  describe('Given an Authenticated User', () => {
+    let normalUser: FactoryUser,
+      superAdminUser: FactoryUser,
+      company: FactoryCompany,
+      normalUserToken: string,
+      superAdminToken: string
+    beforeAll(async () => {
+      company = await FactoryCompany.create({})
+      normalUser = await FactoryUser.create({ companyId: company.id, role: UserRoles.User })
+      normalUserToken = await normalUser.login()
+      superAdminUser = await FactoryUser.create({ companyId: company.id, role: UserRoles.SuperAdmin })
+      superAdminToken = await superAdminUser.login()
+    })
+    afterAll(async () => {
+      await normalUser.delete()
+      await superAdminUser.delete()
+      await company.delete()
+    })
+    describe('When he is a SuperAdmin', () => {
+      describe('And he wants to update a Unit of Another Company', () => {
+        it('should update the Unit', async () => {
+          const newName = faker.commerce.productName()
+          const newDescription = faker.commerce.productDescription()
+          const anotherCompany = await FactoryCompany.create({})
+          const unitFromAnotherCompany = await FactoryUnit.create({ companyId: anotherCompany.id })
+          const response = await request(expressApp).patch(`/unit/${unitFromAnotherCompany.id}`)
+            .send({
+              name: newName,
+              description: newDescription
+            })
+            .set('Authorization', `Bearer ${superAdminToken}`)
+
+          expect(response.statusCode).toBe(200)
+          expect(response.body.id).toBe(unitFromAnotherCompany.id)
+          expect(response.body.name).toBe(newName)
+          expect(response.body.description).toBe(newDescription)
+
+          await unitFromAnotherCompany.delete()
+          await anotherCompany.delete()
+        })
+      })
+    })
+    describe('When he is not a SuperAdmin', () => {
+      describe('And he wants to update a unit from another company', () => {
+        it('Should return a Unit not Found error', async () => {
+          const anotherCompany = await FactoryCompany.create({})
+          const unitFromAnotherCompany = await FactoryUnit.create({ companyId: anotherCompany.id })
+          const response = await request(expressApp).patch(`/unit/${unitFromAnotherCompany.id}`)
+            .send({
+              name: faker.commerce.productName(),
+              description: faker.commerce.productDescription()
+            })
+            .set('Authorization', `Bearer ${normalUserToken}`)
+
+          expect(response.statusCode).toBe(404)
+          expect(response.body.error).toBe(new UnitNotFoundError().message)
+
+          await unitFromAnotherCompany.delete()
+          await anotherCompany.delete()
+        })
+      })
+      describe('When he wants to update a unit from its own company', () => {
+        describe('When the unit does not exist', () => {
+          it('should return a Unit Not Find error', async () => {
+            const response = await request(expressApp).patch('/unit/123')
+              .send({
+                name: faker.commerce.productName(),
+                description: faker.commerce.productDescription()
+              })
+              .set('Authorization', `Bearer ${normalUserToken}`)
+
+            expect(response.statusCode).toBe(404)
+            expect(response.body.error).toBe(new UnitNotFoundError().message)
+          })
+        })
+        describe('When the unit exist', () => {
+          it('should get the Unit', async () => {
+            const newName = faker.commerce.productName()
+            const newDescription = faker.commerce.productDescription()
+            const unit = await FactoryUnit.create({ companyId: company.id })
+            const response = await request(expressApp).patch(`/unit/${unit.id}`)
+              .send({
+                name: newName,
+                description: newDescription
+              })
+              .set('Authorization', `Bearer ${normalUserToken}`)
+
+            expect(response.statusCode).toBe(200)
+            expect(response.body.id).toBe(unit.id)
+            expect(response.body.name).toBe(newName)
+            expect(response.body.description).toBe(newDescription)
+
+            await unit.delete()
+          })
+        })
+      })
+    })
+  })
+  describe('Given an Unauthenticated User', () => {
+    it('should return an unauthorized error', async () => {
+      const response = await request(expressApp).patch('/unit/123')
+        .send({
+          name: faker.commerce.productName(),
+          description: faker.commerce.productDescription()
+        })
+
+      expect(response.statusCode).toBe(401)
+    })
+  })
+})
