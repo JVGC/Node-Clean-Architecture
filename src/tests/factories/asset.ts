21ed6d5b43a34a6608c656c6c4eff2f935fc1ac2
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-06-06 15:02:39 -0300

    Asset and Unit Factory

---
+++ src/tests/factories/asset.ts
@@ -0,0 +1,86 @@
+import { faker } from '@faker-js/faker'
+import { AssetStatus } from '@prisma/client'
+import prisma from '../../infra/prisma/client'
+
+interface FactoryModel {
+  name?: string
+  description?: string
+  model?: string
+  owner?: string
+  status?: AssetStatus
+  healthLevel?: number
+  imageURL?: string
+  unitId: string
+}
+
+export class FactoryAsset {
+  id: string
+  name: string
+  description: string
+  model: string
+  owner: string
+  status: AssetStatus
+  healthLevel: number
+  imageURL?: string
+
+  unitId: string
+
+  constructor (id: string, name: string, description: string,
+    model: string, owner: string, status: AssetStatus,
+    healthLevel: number, imageURL: string, unitId: string
+  ) {
+    this.id = id
+    this.name = name
+    this.description = description
+    this.unitId = unitId
+    this.model = model
+    this.owner = owner
+    this.status = status
+    this.healthLevel = healthLevel
+    this.imageURL = imageURL
+  }
+
+  static async create ({
+    name = faker.person.fullName(),
+    description = faker.internet.password(),
+    model = faker.vehicle.model(),
+    owner = faker.company.name(),
+    status = AssetStatus.Running,
+    healthLevel = 100,
+    imageURL = faker.image.url(),
+    unitId
+  }: FactoryModel): Promise<FactoryAsset> {
+    const prismaUnit = await prisma.asset.create({
+      data: {
+        name,
+        description,
+        model,
+        owner,
+        status,
+        healthLevel,
+        imageURL,
+        unitId
+      }
+    })
+
+    return new FactoryAsset(
+      prismaUnit.id,
+      prismaUnit.name,
+      prismaUnit.description,
+      prismaUnit.model,
+      prismaUnit.owner,
+      prismaUnit.status,
+      prismaUnit.healthLevel,
+      prismaUnit.imageURL ?? '',
+      prismaUnit.unitId
+    )
+  }
+
+  async delete (): Promise<void> {
+    await prisma.asset.delete({
+      where: {
+        id: this.id
+      }
+    })
+  }
+}
---
+++ src/tests/factories/unit.ts
@@ -0,0 +1,51 @@
+import { faker } from '@faker-js/faker'
+import prisma from '../../infra/prisma/client'
+
+interface FactoryModel {
+  name?: string
+  description?: string
+  companyId: string
+}
+
+export class FactoryUnit {
+  id: string
+  name: string
+  description: string
+  companyId: string
+
+  constructor (id: string, name: string, description: string, companyId: string) {
+    this.id = id
+    this.name = name
+    this.description = description
+    this.companyId = companyId
+  }
+
+  static async create ({
+    name = faker.person.fullName(),
+    description = faker.internet.password(),
+    companyId
+  }: FactoryModel): Promise<FactoryUnit> {
+    const prismaUnit = await prisma.unit.create({
+      data: {
+        name,
+        description,
+        companyId
+      }
+    })
+
+    return new FactoryUnit(
+      prismaUnit.id,
+      prismaUnit.name,
+      prismaUnit.description,
+      prismaUnit.companyId
+    )
+  }
+
+  async delete (): Promise<void> {
+    await prisma.unit.delete({
+      where: {
+        id: this.id
+      }
+    })
+  }
+}
